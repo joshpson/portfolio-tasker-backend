357cc3fd0a38524a55f24791fbb96ed395012d1d
joshpson <jpearson846@gmail.com>
2018-06-18 14:38:25 -0400

    task model and controller and migration and working api for tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TasksController < ApplicationController
 
   def index
-    test_data = ["Hello World!"]
-    render json: test_data.to_json
+    @tasks = Task.all
+    render json: @tasks.to_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+    # t.integer "user_id"
+    # t.integer "project_id"
+    # t.date "due_date"
+    # t.integer "priority"
+    # t.string "status"
+    # t.datetime "created_at", null: false
+    # t.datetime "updated_at", null: false
+    # t.string "description"
+
+# Task.create([
+#   {}
+# ])
