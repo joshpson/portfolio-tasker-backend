b6c1a4c4a373627ef23edc641872c23ff8463acc
joshpson <jpearson846@gmail.com>
2018-06-19 17:05:53 -0400

    posting of userprojects

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :tasks, :users, :projects
+      resources :tasks, :users, :projects, :userprojects
       patch "projects/:id" => 'projects#update'
+      post "projects" => 'project#create'
       patch "tasks/:id" => 'tasks#update'
       post "tasks" => 'tasks#create'
+      post "userprojects" => 'userprojects#create'
     end
   end
 
