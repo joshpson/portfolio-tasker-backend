c1a9ebfce8f32f566a130074166d5093151c4a82
joshpson <jpearson846@gmail.com>
2018-07-16 18:12:44 -0400

    more demo fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :projects
   has_many :tasks, through: :projects
   validates :username, uniqueness: true, presence: true
-  after_create :reset_demo
+  after_initialize :reset_demo
 
   def reset_demo
     if self.username === "demo"
