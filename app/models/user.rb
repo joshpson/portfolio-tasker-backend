a55b6dc98e29f72376e426f5fdb9906c6c2b2a13
joshpson <jpearson846@gmail.com>
2018-07-16 18:31:32 -0400

    demo project after create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :projects
   has_many :tasks, through: :projects
   validates :username, uniqueness: true, presence: true
+  after_create :create_demo_projects
 
+  def create_demo_projects
+    demo_project = Project.create({title: "Demo Project"})
+    self.projects <<  demo_project
+  end
 
 end
