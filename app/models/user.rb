ae4f2ae08f886dbf89608519fe2d589b0d83eb08
joshpson <jpearson846@gmail.com>
2018-07-16 18:42:37 -0400

    final fixes to demo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,13 @@ class User < ApplicationRecord
     demo_project = Project.create({title: "Demo Project", status: "Active"})
     task1 = Task.create({ description: "Add a task to this project"})
     task2 = Task.create({ description: "Toggle this task as done"})
-    task3 = Task.create({ description: "Click on the top right options to see completed tasks"})
+    task3 = Task.create({ description: "Click on the project options to see completed tasks"})
     task4 = Task.create({ description: "Uncheck this task to make it active again", status: "Completed"})
-    task5 = Task.create({ description: "Click on the top left button to add a new project"})
-    task6 = Task.create({ description:  "Drag this task to another project"})
+    task5 = Task.create({ description: "Click on the top right button to add a new project"})
+    task6 = Task.create({ description: "Drag this task to another project"})
     task7 = Task.create({ description: "Double click on this task to edit"})
+    task8 = Task.create({ description: "Double click on this project title to edit"})
+    task9 = Task.create({ description: "Happy tasking!"})
     demo_project.tasks << task1
     demo_project.tasks << task2
     demo_project.tasks << task3
@@ -20,6 +22,8 @@ class User < ApplicationRecord
     demo_project.tasks << task5
     demo_project.tasks << task6
     demo_project.tasks << task7
+    demo_project.tasks << task8
+    demo_project.tasks << task9
     self.projects << demo_project
   end
 
