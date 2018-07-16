c5968880537bdb86ba68cec0da1c0d1a05a6a4a2
joshpson <jpearson846@gmail.com>
2018-07-16 18:03:53 -0400

    updating code for demo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   has_many :projects
   has_many :tasks, through: :projects
   validates :username, uniqueness: true, presence: true
+  after_create :reset_demo
+
+  def reset_demo
+    if self.username === "demo"
+      self.projects.clear
+      demo_project = Project.create({title: "Demo Project", user: self})
+      task1 = Task.create({description: "Add a task to this project"})
+      demo_project.tasks << task1
+    end
+  end
+
 end
