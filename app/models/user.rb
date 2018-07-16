5dc6bf6fac62f50c82c2ad949598631c28e8cd1d
joshpson <jpearson846@gmail.com>
2018-07-16 18:20:58 -0400

    demo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,6 @@ class User < ApplicationRecord
   has_many :projects
   has_many :tasks, through: :projects
   validates :username, uniqueness: true, presence: true
-  after_initialize :reset_demo
 
-  def reset_demo
-    if self.username === "demo"
-      self.projects.clear
-      demo_project = Project.create({title: "Demo Project", user: self})
-      task1 = Task.create({description: "Add a task to this project"})
-      demo_project.tasks << task1
-    end
-  end
 
 end
