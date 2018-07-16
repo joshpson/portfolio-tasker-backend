de993895b3fd838266e2d481bbcbb412def45b40
joshpson <jpearson846@gmail.com>
2018-07-16 18:38:24 -0400

    more fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,17 +9,18 @@ class User < ApplicationRecord
     task1 = Task.create({ description: "Add a task to this project"})
     task2 = Task.create({ description: "Toggle this task as done"})
     task3 = Task.create({ description: "Click on the top right options to see completed tasks"})
-    task4 = Task.create({ description: "Uncheck this task to make it active again",  status: "Completed"})
+    task4 = Task.create({ description: "Uncheck this task to make it active again", status: "Completed"})
     task5 = Task.create({ description: "Click on the top left button to add a new project"})
     task6 = Task.create({ description:  "Drag this task to another project"})
     task7 = Task.create({ description: "Double click on this task to edit"})
-    self.projects << task1
-    self.projects << task2
-    self.projects << task3
-    self.projects << task4
-    self.projects << task5
-    self.projects << task6
-    self.projects << task7
+    demo_project.tasks << task1
+    demo_project.tasks << task2
+    demo_project.tasks << task3
+    demo_project.tasks << task4
+    demo_project.tasks << task5
+    demo_project.tasks << task6
+    demo_project.tasks << task7
+    self.projects << demo_project
   end
 
 end
